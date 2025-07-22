56f76523ae1e899f09b9a9339cfac49d632a057a
MaximOdincov <124523356+MaximOdincov@users.noreply.github.com>
2025-07-22 11:30:20 +0500

    initial commit

---
+++ app/src/main/java/com/example/shoppinglist/domain/EditItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglist.domain
+
+class EditItemUseCase {
+    fun editItem(item: ShopItem) {
+        TODO()
+    }
+
+}
---
+++ app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
@@ -0,0 +1,3 @@
+package com.example.shoppinglist.domain
+
+data class ShopItem(val name: String, val id: Int, val count: Int, val enabled: Boolean)
